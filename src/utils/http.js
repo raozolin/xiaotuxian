cfaedaa129b09794a958e35ede4ae3b588b95464
raozilin <1565865297@qq.com>
2023-10-27 19:24:42 +0800

    在拦截器里面设置统一的错误提示

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
 const httpIntance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -17,6 +19,11 @@ httpIntance.interceptors.request.use(
 httpIntance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    // 统一错误提示
+    ElMessage({
+      type: "warning",
+      message: e.response.data.message,
+    });
     return Promise.reject(e);
   }
 );
