200fd8e915debf832807d77e68d6368096f1ed94
raozilin <1565865297@qq.com>
2023-10-27 20:58:17 +0800

    401 token失效处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { useUserStore } from "@/stores/user.js";
+import router from "@/router";
 const httpIntance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -29,12 +30,23 @@ httpIntance.interceptors.request.use(
 httpIntance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    const userStore = useUserStore();
+
     // 统一错误提示
     // 只要配置一次，就可以使得多个接口生效
     ElMessage({
       type: "warning",
       message: e.response.data.message,
     });
+    // 401 token失效处理
+
+    if (e.response.status === 401) {
+      // 1.清除本地用户数据
+      userStore.clearUserInfo();
+
+      // 2.跳转到登录页
+      router.push("/login");
+    }
     return Promise.reject(e);
   }
 );
